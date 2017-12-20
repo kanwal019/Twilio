fb76e099c0f82a58765aba90f76b2d447bae0612
kanwal019 <kanwal019@gmail.com>
2017-12-21 00:01:30 +0530

    Add project files.

---
+++ TwilioWork/Controllers/TwilioMessageController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace TwilioWork.Controllers
+{
+    public class TwilioMessageController : Controller
+    {
+        // GET: TwilioMessage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string SendMessage(string toNumber, string messageDesc)
+        {
+            try
+            {
+                string accountSid = System.Configuration.ConfigurationManager.AppSettings["ClientId"];
+                string authToken = System.Configuration.ConfigurationManager.AppSettings["ClientSecret"];
+                string fromNumber = System.Configuration.ConfigurationManager.AppSettings["PhoneNumber"];
+                TwilioClient.Init(accountSid, authToken);
+
+                var to = new PhoneNumber(toNumber);
+                var message = MessageResource.Create(to, from: new PhoneNumber(fromNumber), body: messageDesc);
+
+                return ("Message Sent : " + message.Sid + " !!");
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
